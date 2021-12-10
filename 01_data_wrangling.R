b3fde996146adee506fc6c35c5076d66515d802b
Kendra Lyons <kendrdamlyons@email.arizona.edu>
2021-12-09 23:24:22 -0700

    save wrangling script

--- 01_data_wrangling.R
+++ 01_data_wrangling.R
@@ -12,6 +12,6 @@ census_voters_all_incomes <- read_csv("data/census_voters_all_incomes.csv")
 
 nonvoter_reasons_by_income <- read_csv("data/nonvoter_reasons_by_income.csv")
 
-voter_survey_538 <- read_csv("data/raw_data/voter_survey_538.csv")                                      
+voter_survey_538 <- read_csv("data/raw_data/nonvoters_538.csv")                                      
 
                                      
